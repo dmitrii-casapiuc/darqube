ae592299d018d800c9f8ab5572bf9fec6a1af640
Dima Casapciuc <dmitrii.casapciuc@feel-it-services.com>
2021-04-07 23:06:27 +0300

    added responsive

--- src/pages/Bookmarks/index.tsx
+++ src/pages/Bookmarks/index.tsx
@@ -1,26 +1,15 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {useSelector} from 'react-redux'
 import {isEmpty} from 'lodash'
 
 import Card from 'components/Card'
-import CustomPagination from 'components/Pagination'
 import {RootState} from 'store/reducers/rootReducer'
 import { INews } from 'interfaces/news.interface'
 
 import './style.scss'
 
 const Bookmarks: React.FC = (): JSX.Element => {
-  const [currentPage, setCurrentPage] = useState<number>(1)
-  const [newsPerPage] = useState<number>(8)
   const {bookmarks} = useSelector((state: RootState) => state.bookmarksState)
-
-  const handlePageClick = (page: number): void => {
-    setCurrentPage(page)
-  }
-  
-  const indexOfLastNews = currentPage * newsPerPage
-  const indexOfFirstNews = indexOfLastNews - newsPerPage
-  const currentNews = bookmarks.slice(indexOfFirstNews, indexOfLastNews)
 
   return (
     <div className="bookmarks">
@@ -31,21 +20,9 @@ const Bookmarks: React.FC = (): JSX.Element => {
             <div className="content">
               <div className="all-bookmarks">
                 {
-                  currentNews.map((item: INews): JSX.Element => <Card key={item.id} item={item} size="small" type="default" />)
+                  bookmarks.map((item: INews): JSX.Element => <Card key={item.id} item={item} size="small" type="default" />)
                 }
               </div>
-              {
-                bookmarks.length > 8
-                  ? (
-                    <CustomPagination
-                      handlePageClick={handlePageClick}
-                      current={currentPage}
-                      total={bookmarks.length}
-                      defaultPageSize={newsPerPage}
-                    />
-                  )
-                  : ''
-              }
             </div>
           )
       }
